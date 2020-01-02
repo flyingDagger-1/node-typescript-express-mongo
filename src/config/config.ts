70eeecc7a03bf31c68ac2d1ae98ebc9ed8454794
Shrujal Shah <shrujal@peerbits.com>
2020-01-02 12:21:14 +0530

    Prepare for auth module

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,19 @@
-import { CleanEnv, str, port, url, cleanEnv } from 'envalid';
+import { CleanEnv, str, port, url, cleanEnv, makeValidator, num } from 'envalid';
 
 type Environment = {
   NODE_ENV: string;
   PORT: number;
   MONGO_URL: string;
+  SECRET_HEX: string;
+  ACCESS_TOKEN_LIFETIME_MIN: number;
 };
+
+const strHex64 = makeValidator<string>(x => {
+  if (/^[0-9a-f]{64}$/.test(x)) {
+    return x;
+  }
+  throw new Error('Expected a hex-character string of length 64');
+});
 
 export type Config = Readonly<Environment & CleanEnv>;
 
@@ -12,6 +21,8 @@ const config: Config = cleanEnv<Environment>(process.env, {
   NODE_ENV: str({ choices: ['production', 'test', 'development'] }),
   PORT: port(),
   MONGO_URL: url(),
+  SECRET_HEX: strHex64(),
+  ACCESS_TOKEN_LIFETIME_MIN: num(),
 });
 
 export default config;
